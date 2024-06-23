5139ace65d74d0ca84afba04c66cae4a6f963051
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2024-06-23 22:06:57 +0200

    ex9 - different ways to us print statements in python

---
+++ ex9.py
@@ -0,0 +1,18 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+
+# \n tells python to write the input on a new line. 
+
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# you can print a block of text by using three double quotation marks instead of 
+# one in your print statements
+
+print("""
+      There's something going on here.
+      With the three double quotes
+      We'll be able to type as much as we like. 
+      Even 4 lines if we want, or 5, or 6. 
+      """)
