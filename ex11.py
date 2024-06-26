91a89704ee3193c9609de017fd5ac67c8ab6ed11
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2024-06-26 20:25:58 +0200

    ex 11 and 12 with comments

--- ex11.py
+++ ex11.py
@@ -1,8 +1,11 @@
+# Printing quuestions and asking the user for an input
 print("How old are you?", end='')
 age = input()
 print("How tall are you?", end='')
 height = input()
 print("How much do you weigh?", end='')
 weight = input()
+
+# Printing inputs to a string using formatter. 
 
 print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
---
+++ ex12.py
@@ -0,0 +1,9 @@
+# Printing questions and taking inputs in the same line of code. 
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+# Printing variables to string with formatter
+
+print("So, you're {age} old, {height} tall and, {weight} heavy")
