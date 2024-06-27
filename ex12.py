de810db7400dd050160f83ca1aaf7837c1c15815
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2024-06-27 17:35:05 +0200

    Ex12 with comments

--- ex12.py
+++ ex12.py
@@ -4,6 +4,7 @@ age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
-# Printing variables to string with formatter
+# Printing variables to string with formatter. 
+# If you exclude the 'f' the string will print out the names of the variables in the braces.
 
-print("So, you're {age} old, {height} tall and, {weight} heavy")
+print(f"So, you're {age} old, {height} tall and, {weight} heavy")
