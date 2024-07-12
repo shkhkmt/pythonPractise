c99fb807761a7c5631fbd6e969ec86428b505e3e
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2024-07-12 21:42:50 +0200

    Reading text files - ex15.

--- ex14.py
+++ ex14.py
@@ -1,10 +1,14 @@
 from sys import argv 
 
-script, userName = argv
+script, userName, pseudonym = argv
 prompt = '> '
 
 print(f"Hi {userName}, I'm the {script} script.")
-print("I'd like to ask you a few questions.") 
+print(f"Do you have a nickname?")
+nickName = input(prompt)
+print(f"{nickName}? that's cool. I wonder how you got it?")
+backStory = input(prompt)
+
 print(f"Do you like me {userName}?") 
 likes = input(prompt) 
 
@@ -16,8 +20,9 @@ print("What kind of computer do you have?")
 computer = input(prompt) 
 
 print(f"""
-Alright, so you said {likes} about liking me. 
-You live in {lives}. Not sure where that is. 
-And you have a {computer} computer. Nice. 
+Alright, so you said {likes} about liking me. You said you go by, {nickName}.
+And that you got this name from {backStory}.
+You live in {lives}. South Africa's biggest city. 
+And you have a {computer} computer. Which isn't relevant to anything. 
 """)
 
